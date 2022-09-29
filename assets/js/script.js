b3617bfec987e885baa90973213f0969a8d8a30f
saif <saifalis@axioned.com>
2022-09-29 18:47:36 +0530

    accordion function code completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
-/* Author: 
+// Accordion function
+var accordionList = document.querySelectorAll('.accordion-list');
 
-*/
+// accordionList.forEach(function (list) {
+//   list.addEventListener('click',function () {
+//     var activeList = document.querySelector('.active-list');
+//     list.classList.toggle('active-list');
+//   });
+// });
+
+accordionList.forEach(function (list) {
+  list.addEventListener('click',function () {
+    var activeList = document.querySelector('.active-list');
+    activeList.classList.remove('active-list');
+    list.classList.add('active-list');
+  });
+});
 
 
 
